074c603b739ac735398e1445ca79aaeaab081e90
juanchi0207 <juanchi.sampieri@gmail.com>
2025-05-11 16:35:31 -0300

    agregue cors para las peticiones desde el front

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,18 @@ dotenv.config({
 
 import express from 'express';
 import generateRouter from './routes/generate';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173',      // o '*' para pruebas
+  methods: ['GET','POST','OPTIONS'],    
+  allowedHeaders: ['Content-Type'],     
+  credentials: false                     // cambia a true si usas cookies/autenticación
+}));
 app.use(express.json());
 app.use('/generate', generateRouter); 
+
+
 
 export default app;
